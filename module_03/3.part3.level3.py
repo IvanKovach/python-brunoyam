97a193ce0b3e90eb69ef99ac04c7f9d91c3dea90
Ivan Kovach <ivan.kovach@x5.ru>
2022-02-20 22:01:39 +0300

    Part 3 level 3 Done

--- module_03/3.part3.level3.py
+++ module_03/3.part3.level3.py
@@ -5,22 +5,25 @@ numbers = [randint(0, 100) for i in range(n)]
 
 
 def calc_maximum(list_numbers):
+    #Сортируем список от больщего к меньшему
     list_numbers.sort(reverse=True)
-    result = ""
+    #В отсортированном списке перемещаем числа из 1 цифры вверх - ставим перед числами с тем же десятком
+    #Например, цифру 2 ставим перед числами 20, 21, 22 .. и т.д.
     for i in list_numbers:
-        if result == "":
-            result = str(i)
-            continue
-        if int(str(i)[0]) >= int(result[0]):
-            result = str(i) + result
-        else:
-            result = result + str(i)
+        if len(str(i)) == 1:
+            for a in list_numbers:
+                if i >= int(str(a)[0]) and i != a:
+                    list_numbers.remove(i)
+                    list_numbers.insert(list_numbers.index(a), i)
+                    break
+    result = "".join(str(b) for b in list_numbers)
     return result
 
 
 numbers1 = [56, 9, 11, 2]
 numbers2 = [3, 81, 5]
-print(numbers)
-print(f"Максимальное число: {calc_maximum(numbers)}")
-print(f"Максимальное число: {calc_maximum(numbers1)}")
-print(f"Максимальное число: {calc_maximum(numbers2)}")
+numbers3 = [2, 12, 21, 91, 9]
+print(f"Из массива {numbers} можно составить максимальное число: {calc_maximum(numbers)}")
+print(f"Из массива {numbers1} можно составить максимальное число: {calc_maximum(numbers1)}")
+print(f"Из массива {numbers2} можно составить максимальное число: {calc_maximum(numbers2)}")
+print(f"Из массива {numbers3} можно составить максимальное число: {calc_maximum(numbers3)}")
